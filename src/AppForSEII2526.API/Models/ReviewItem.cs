7bc81241d037600bb0f4c657ce84b9442003f4aa
rodrigo <rodrigo.abril@alu.uclm.es>
2025-10-03 09:31:18 +0200

    Creacion de la clase reviewItem y completada

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppForSEII2526.API.Models
+{
+    public class ReviewItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int Rating { get; set; }
+
+        [StringLength(500, ErrorMessage = "Comment cannot exceed 500 characters.")]
+        [Required]
+        public string Comments { get; set; }
+
+        // Relación con Device
+        [ForeignKey("Device")]
+        public int DeviceId { get; set; }
+        public Device Device { get; set; }
+
+        // Relación con Review
+        [ForeignKey("Review")]
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
+    }
+}
