02455414c94e180836ef9847dd463b6f3e013dcd
M <miguelamorenc@gmail.com>
2025-10-08 18:41:25 +0200

    Añado constructores a la clase Rental.

--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -4,6 +4,28 @@
 
     public class Rental
     {
+        // Constructor vacio
+        public Rental()
+        {
+        }
+
+        //Constructor con parametros sin id
+        public Rental(string deliveryAddress, PaymentMethodType paymentMethod, DateTime rentalDate, DateTime rentalDateFrom, DateTime rentalDateTo, double totalPrice)
+        {
+            DeliveryAddress = deliveryAddress;
+            PaymentMethod = paymentMethod;
+            RentalDate = rentalDate;
+            RentalDateFrom = rentalDateFrom;
+            RentalDateTo = rentalDateTo;
+            TotalPrice = totalPrice;
+        }
+
+        //Constructor con parametros con id
+        public Rental(int id, string deliveryAddress, PaymentMethodType paymentMethod, DateTime rentalDate, DateTime rentalDateFrom, DateTime rentalDateTo, double totalPrice) : this(deliveryAddress, paymentMethod, rentalDate, rentalDateFrom, rentalDateTo, totalPrice)
+        {
+            Id = id;
+        }
+
         [Required]
         [StringLength(100, ErrorMessage = "El campo DeliveryAddress no puede ser mayor a 100 caracteres ni menor de 1 caracter.", MinimumLength = 1)]
         public string DeliveryAddress { get; set; }
@@ -11,9 +33,9 @@
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(50, ErrorMessage = "El campo Name no puede ser mayor a 50 caracteres ni menor de 1 caracter.", MinimumLength=1)]
-        public string Name { get; set; }
+        //[Required]
+        //[StringLength(50, ErrorMessage = "El campo Name no puede ser mayor a 50 caracteres ni menor de 1 caracter.", MinimumLength=1)]
+        //public string Name { get; set; }
 
         [Required]
         public PaymentMethodType PaymentMethod { get; set; }
@@ -30,9 +52,9 @@
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime RentalDateTo { get; set; }
 
-        [Required]
-        [StringLength(50, ErrorMessage = "El campo Surname no puede ser mayor a 50 caracteres ni menor de 1 caracter.", MinimumLength = 1)]
-        public string Surname { get; set; }
+        //[Required]
+        //[StringLength(50, ErrorMessage = "El campo Surname no puede ser mayor a 50 caracteres ni menor de 1 caracter.", MinimumLength = 1)]
+        //public string Surname { get; set; }
 
         [DataType(DataType.Currency)]
         [Display(Name = "Precio Total")]
@@ -40,7 +62,7 @@
         public double TotalPrice { get; set; }
 
         //Relacion uno a muchos con RentDevice
-        public RentDevice rentDevice { get; set; }
+        public RentDevice RentDevice { get; set; }
 
 
     }
