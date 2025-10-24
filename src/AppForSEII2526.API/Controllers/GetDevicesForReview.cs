2d1306c869954bc11314f299a1521ecac7351300
rodrigo <rodrigo.abril@alu.uclm.es>
2025-10-24 12:58:03 +0200

    cambio al nombre del controlador dispositivos para reseñar

--- src/AppForSEII2526.API/Controllers/GetDevicesForReview.cs
+++ src/AppForSEII2526.API/Controllers/GetDevicesForReview.cs
@@ -9,14 +9,14 @@ namespace AppForSEII2526.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class reseñaDevicesController : ControllerBase
+    public class GetDevicesForReview : ControllerBase
     {
         //used to enable your controller to access to the database
         private readonly ApplicationDbContext _context;
         //used to log any information when your system is running
-        private readonly ILogger<reseñaDevicesController> _logger;
+        private readonly ILogger<GetDevicesForReview> _logger;
 
-        public reseñaDevicesController(ApplicationDbContext context, ILogger<reseñaDevicesController> logger)
+        public GetDevicesForReview(ApplicationDbContext context, ILogger<GetDevicesForReview> logger)
         {
             _context = context;
             _logger = logger;
