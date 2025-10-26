4054059d6ca8dc8439ad4972e13e317ffcde9755
rodrigo <rodrigo.abril@alu.uclm.es>
2025-10-26 14:44:42 +0100

    ReviewDTO

---
+++ src/AppForSEII2526.API/DTOs/ReseñasDTOs/ReviewDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace AppForSEII2526.API.DTOs.ReseñasDTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+    public class ReviewItemDetailsDTO
+    {
+        public string DeviceName { get; set; }
+        public string DeviceModel { get; set; }
+        public int DeviceYear { get; set; }
+        public int Rating { get; set; } 
+        public string? Comment { get; set; } 
+    }
+
+    
+    public class ReviewDetailsDTO
+    {
+        public string CustomerName { get; set; } 
+        public string CustomerCountry { get; set; }
+        public string ReviewTitle { get; set; } 
+        public DateTime DateOfReview { get; set; } 
+        public IList<ReviewItemDetailsDTO> ReviewItems { get; set; } = new List<ReviewItemDetailsDTO>();
+    }
+
+
+
