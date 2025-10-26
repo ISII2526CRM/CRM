ff38a2921f957e615e3290093f88aa53b02dfa88
rodrigo <rodrigo.abril@alu.uclm.es>
2025-10-26 12:13:13 +0100

    cambio nombre erroneo GetDevices

--- src/AppForSEII2526.API/Controllers/GetDevices.cs
+++ src/AppForSEII2526.API/Controllers/GetDevices.cs
@@ -9,14 +9,14 @@ namespace AppForSEII2526.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GetDevicesForReview : ControllerBase
+    public class GetDevices : ControllerBase
     {
         //used to enable your controller to access to the database
         private readonly ApplicationDbContext _context;
         //used to log any information when your system is running
-        private readonly ILogger<GetDevicesForReview> _logger;
+        private readonly ILogger<GetDevices> _logger;
 
-        public GetDevicesForReview(ApplicationDbContext context, ILogger<GetDevicesForReview> logger)
+        public GetDevices(ApplicationDbContext context, ILogger<GetDevices> logger)
         {
             _context = context;
             _logger = logger;
