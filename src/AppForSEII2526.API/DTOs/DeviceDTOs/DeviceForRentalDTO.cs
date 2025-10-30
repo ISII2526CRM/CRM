fd9b4809ebb8735453d7dd170c40b09d652de56e
M <miguelamorenc@gmail.com>
2025-10-30 19:05:03 +0100

    Creación de le metodo GetDevicesForRental y la clase DeviceForRentalDTO

---
+++ src/AppForSEII2526.API/DTOs/DeviceDTOs/DeviceForRentalDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace AppForSEII2526.API.DTOs.DeviceDTOs
+{
+	public class DeviceForRentalDTO
+	{
+		public DeviceForRentalDTO()
+		{
+		}
+		public DeviceForRentalDTO(int id, string brand, string color, int year, string modelName, decimal priceForRent)
+		{
+			Id = id;
+			Brand = brand;
+			Color = color;
+			Year = year;
+			ModelName = modelName;
+			PriceForRent = priceForRent;
+		}
+
+		public int Id { get; set; }
+		public string Brand { get; set; } = string.Empty;
+		public string Color { get; set; } = string.Empty;
+		public int Year { get; set; }
+		public string ModelName { get; set; } = string.Empty;
+		public decimal PriceForRent { get; set; }
+	}
+}
