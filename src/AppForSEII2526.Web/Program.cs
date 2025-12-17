1617b3b1f0f53ff73a4bbcf91bbe4c64e89d2c24
rodrigo <rodrigo.abril@alu.uclm.es>
2025-12-17 19:33:26 +0100

    cambios development

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIC
 //adding an In-memory state container service
 //https://learn.microsoft.com/en-us/aspnet/core/blazor/state-management/?view=aspnetcore-8.0#in-memory-state-container-service
 builder.Services.AddScoped<ReviewStateContainer>();
+builder.Services.AddScoped<RentalStateContainer>();
 
 var app = builder.Build();
 
